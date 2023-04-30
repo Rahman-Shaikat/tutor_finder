63e330fabc7dccab03678ecd7503b0dd2d89daae
shaikat <shaikat3257@gmail.com>
2023-04-30 23:27:45 +0600

    added

--- resources/views/admin-layouts/includes/sidebar.blade.php
+++ resources/views/admin-layouts/includes/sidebar.blade.php
@@ -49,12 +49,12 @@ $tutor_req_count=App\Models\User::where('is_tutor', 1)->where('status', 2)->coun
     </li>
 
     <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
+    <!-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-wrench"></i>
             <span>Settings</span>
         </a>
-        <!-- <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Utilities:</h6>
                 <a class="collapse-item" href="utilities-color.html">Colors</a>
@@ -62,8 +62,8 @@ $tutor_req_count=App\Models\User::where('is_tutor', 1)->where('status', 2)->coun
                 <a class="collapse-item" href="utilities-animation.html">Animations</a>
                 <a class="collapse-item" href="utilities-other.html">Other</a>
             </div>
-        </div> -->
-    </li>
+        </div>
+    </li> -->
 
     <!-- Divider -->
     <hr class="sidebar-divider">
